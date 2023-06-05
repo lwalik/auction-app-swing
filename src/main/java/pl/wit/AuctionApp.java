e9aa08847f9659141632b8519e1029b4ff480520
lwalik <l.walicki1@gmail.com>
2023-06-05 17:12:07 +0200

    feat: code improved

--- src/main/java/pl/wit/AuctionApp.java
+++ src/main/java/pl/wit/AuctionApp.java
@@ -3,78 +3,124 @@ package pl.wit;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class AuctionApp {
     private final JFrame frame = new JFrame("Auction App");
     private final CardLayout cardLayout = new CardLayout();
     private final JPanel cards = new JPanel(cardLayout);
     BufferedReader in;
-//    private Map<Integer, Product> products;
+    private Map<Integer, Product> products = new HashMap<>();
+    private Socket socket;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
 
-
-    private List<Product> products = Arrays.asList(
-            new Product("Product 1", 100.00, 500.00, "/images/pobrane.png"),
-            new Product("Product 2", 85.00, 170.00, "/images/apple-iphone-xs.jpg"),
-            new Product("Product 3", 15.00, 45.00,  "/images/apple-iphone-xs.jpg")
-    );
+//    private List<Product> products = Arrays.asList(
+//            new Product("Product 1", 100.00, 500.00, "/images/pobrane.png"),
+//            new Product("Product 2", 85.00, 170.00, "/images/apple-iphone-xs.jpg"),
+//            new Product("Product 3", 15.00, 45.00,  "/images/apple-iphone-xs.jpg")
+//    );
     public AuctionApp() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,800);
-
-        this.generateCards(products);
     }
 
     public static void main(String[] args) throws Exception {
         AuctionApp client = new AuctionApp();
-        client.run2();
+        client.connect();
+        client.run();
     }
 
     public void run(){
-        frame.setVisible(true);
-    }
-
-    public void run2() throws IOException, ClassNotFoundException {
-        Socket socket = new Socket("localhost", 9001);
-        ObjectInputStream inObject = new ObjectInputStream(socket.getInputStream());
-        in = new BufferedReader(new InputStreamReader(
-                socket.getInputStream()));
-
-//        Map<Integer, Product> products = (Map<Integer, Product>) inObject.readObject();
-//
-//        for (Map.Entry<Integer, Product> entry : products.entrySet()) {
-//            int key = entry.getKey();
-//            Product value = entry.getValue();
-//            System.out.println("Key: " + key + ", Value: " + value.getName());
-//        }
-
+        this.sendRequest("GET_ALL");
         while (true) {
-            System.out.println("in: " + in.readLine());
+            if (products != null && this.updateProducts()) {
+                this.generateCards(products);
+                frame.setVisible(true);
+            }
         }
     }
 
 
-    private void generateCards(List<Product> list) {
-        for (int i = 0; i < list.size(); i++) {
+    public void connect() {
+        try {
+            socket = new Socket("localhost", 9001);
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Connected to server.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendRequest(String request) {
+        try {
+            outputStream.writeObject(request);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean updateProducts() {
+        try {
+            Map<Integer, Product> response = (Map<Integer, Product>) inputStream.readObject();
+            if (!products.equals(response)) {
+                products = response;
+                System.out.println("Aktualizacja: " + products);
+                return true;
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+        return false;
+    }
+
+
+//    public void disconnect() {
+//        try {
+//            if (outputStream != null)
+//                outputStream.close();
+//            if (inputStream != null)
+//                inputStream.close();
+//            if (socket != null)
+//                socket.close();
+//            System.out.println("Disconnected from server.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+
+    private void generateCards(Map<Integer, Product> list) {
+        int idx = 0;
+        for (Map.Entry<Integer, Product> entry: list.entrySet()) {
+            System.out.println("Produkt: " + entry.getValue().getName());
             List<JButton> buttons = new ArrayList<>();
-            if (i != 0) {
+            if (idx != 0) {
                 buttons.add(new JButton(backAction));
             }
 
-            if (i != list.size() - 1) {
+            if (idx != list.size() - 1) {
                 buttons.add(new JButton(nextAction));
             }
 
             JPanel card = buttons.size() > 1
-                    ? this.createCard(list.get(i), buttons.get(0), buttons.get(1))
-                    : this.createCard(list.get(i), buttons.get(0));
-            cards.add(card, "Page " + i);
+                    ? this.createCard(entry.getValue(), buttons.get(0), buttons.get(1))
+                    : this.createCard(entry.getValue(), buttons.get(0));
+            cards.add(card, "Page " + idx);
+            idx++;
+
         }
+
+
+
 
         frame.getContentPane().add(cards);
     }
@@ -82,7 +128,7 @@ public class AuctionApp {
     private JPanel createCard(Product product, JButton... buttons) {
         JPanel card = new JPanel(new GridLayout(3,2));
 
-        JLabel image = new JLabel(product.getImage(), SwingConstants.CENTER);
+//        JLabel image = new JLabel(product.getImage(), SwingConstants.CENTER);
         JLabel name = new JLabel("Nazwa: " + product.getName(), SwingConstants.CENTER);
         JLabel buyNowPrice = new JLabel("Cena (Kup Teraz): " + product.getBuyNowPriceAsString(), SwingConstants.CENTER);
         JLabel currPrice = new JLabel("Aktualna cena (Licytacja): " + product.getCurrPriceAsString(), SwingConstants.CENTER);
@@ -90,7 +136,7 @@ public class AuctionApp {
 
         JPanel info = new JPanel(new GridLayout(4,1));
 
-        card.add(image);
+//        card.add(image);
 
         info.add(name);
         info.add(buyNowPrice);
@@ -126,4 +172,5 @@ public class AuctionApp {
             cardLayout.previous(cards);
         }
     };
+
 }
--- src/main/java/pl/wit/Product.java
+++ src/main/java/pl/wit/Product.java
@@ -1,14 +1,16 @@
 package pl.wit;
 
 import javax.swing.*;
+import java.io.Serializable;
 import java.net.URL;
 
-public class Product {
+public class Product implements Serializable {
     private final String name;
     private final double buyNowPrice;
-    private final ImageIcon image;
+//    private final ImageIcon image;
     private final double currPrice;
     private final String currBuyer;
+    private static final long serialVersionUID = -6024934201418331673L;
 
     public Product(String name, double currPrice, double buyNowPrice, String imagePath) {
         this.name = name;
@@ -18,11 +20,11 @@ public class Product {
 
         URL url = this.getClass().getResource(imagePath);
 
-        if (url != null) {
-            this.image = new ImageIcon(url);
-        } else {
-            this.image = new ImageIcon("/images/pobrane.png");
-        }
+//        if (url != null) {
+//            this.image = new ImageIcon(url);
+//        } else {
+//            this.image = new ImageIcon("/images/pobrane.png");
+//        }
     }
 
     public String getName() {
@@ -37,9 +39,9 @@ public class Product {
         return Double.toString(this.buyNowPrice);
     }
 
-    public ImageIcon getImage() {
-        return this.image;
-    }
+//    public ImageIcon getImage() {
+//        return this.image;
+//    }
 
     public String getCurrBuyer() {
         return this.currBuyer;
